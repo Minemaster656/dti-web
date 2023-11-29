c0c88e9121f6c610eaf7fa0d514e05cd1f3452a4
minemaster <glebbasmanov123@gmail.com>
2023-11-29 11:55:29 +0400

    index.html

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const http = require('http')
+const fs = require('fs')
 
 
 let server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
-    res.end('Micha <b>Clown</b>')
+    
+
+    const stream = fs.createReadStream('./templates/index.html')
+    stream.pipe(res)
 
 })
 
-const PORT = 3000
+const PORT = 443
 const HOST = 'localhost' //127.0.0.1
 
 server.listen(PORT, HOST, ()=>{
