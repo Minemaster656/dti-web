30b6ba24a7552569db0dca0e5020cdac87ac7758
minemaster <glebbasmanov123@gmail.com>
2023-11-29 12:44:25 +0400

    сносим прод

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const http = require('http')
 const fs = require('fs')
+const express = require('express')
+const app = express()
 
 
-let server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+
+
+
+
+// let server = http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
     
-    if(req.url=='/')
-        fs.createReadStream('./templates/index.html').pipe(res)
-    else if (req.url=='/about')
-        fs.createReadStream('./templates/about.html').pipe(res)
-    else
-        fs.createReadStream('./templates/error.html').pipe(res)
+//     if(req.url=='/')
+//         fs.createReadStream('./templates/index.html').pipe(res)
+//     else if (req.url=='/about')
+//         fs.createReadStream('./templates/about.html').pipe(res)
+//     else
+//         fs.createReadStream('./templates/error.html').pipe(res)
 
-})
+// })
 
 const PORT = 443
 const HOST = 'localhost' //127.0.0.1
 
-server.listen(PORT, HOST, ()=>{
+// server.listen(PORT, HOST, ()=>{
 
-    console.log(`Сервер запущен по адресу http://${HOST}:${PORT}`)
-})
+//     console.log(`Сервер запущен по адресу http://${HOST}:${PORT}`)
+// })
 
